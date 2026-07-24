30ad405d700f76b7dc6e9597a49355324cdde8f4
Bugfunbug <bugfunbug@gmail.com>
2026-07-24 12:09:33 -0400

    Update api-client.ts

--- src/api-client.ts
+++ src/api-client.ts
@@ -4,16 +4,18 @@ import { CacheManager } from "./cache-manager"
 export class ApiClient {
 
 	static Modrinth = new ApiClient("https://api.modrinth.com/v2/project/")
-	static CurseForge = new ApiClient("https://api.curseforge.com/v1/mods/", { "x-api-key": process.env.CF_API_KEY })
+	static CurseForge = new ApiClient("https://api.curseforge.com/v1/mods/", { "x-api-key": process.env.CF_API_KEY }, true)
 	static CFWidget = new ApiClient("https://api.cfwidget.com/")
 
 	url: string
 	headers: Headers
 	rateLimitReset: number | null
 	rateLimitRemaining: number | null
+	trailingSlash: boolean
 
-	constructor(url: string, headers?: object) {
+	constructor(url: string, headers?: object, trailingSlash = false) {
 		this.url = url
+		this.trailingSlash = trailingSlash
 		this.headers = new Headers({
 			"Content-Type": "application/json",
 			"Accept": "application/json",
@@ -25,40 +27,40 @@ export class ApiClient {
 	}
 
 	async get(path: string) {
-    	const fullPath = this.url + path.replace(/\/?$/, "/")
-    	const cachedResult = CacheManager.get(fullPath)
+		const fullPath = this.url + (this.trailingSlash ? path.replace(/\/?$/, "/") : path)
+		const cachedResult = CacheManager.get(fullPath)
 
-    	if (cachedResult !== null) {
-        	return cachedResult
-    	}
+		if (cachedResult !== null) {
+			return cachedResult
+		}
 
-    	if (this.rateLimitRemaining !== null && this.rateLimitReset !== null &&
-        	this.rateLimitRemaining < 1 && Date.now() < this.rateLimitReset) {
-        	throw new Error("third-party rate-limit reached")
-    	}
+		if (this.rateLimitRemaining !== null && this.rateLimitReset !== null &&
+			this.rateLimitRemaining < 1 && Date.now() < this.rateLimitReset) {
+			throw new Error("third-party rate-limit reached")
+		}
 
-    	console.log("URL:", fullPath);
-    	console.log("CF_API_KEY exists:", process.env.CF_API_KEY !== undefined);
-    	console.log("CF_API_KEY length:", process.env.CF_API_KEY?.length);
-    	console.log("Header:", this.headers.get("x-api-key"));
+		console.log("URL:", fullPath);
+		console.log("CF_API_KEY exists:", process.env.CF_API_KEY !== undefined);
+		console.log("CF_API_KEY length:", process.env.CF_API_KEY?.length);
+		console.log("Header:", this.headers.get("x-api-key"));
 
-    	const response = await fetch(fullPath, { headers: this.headers })
+		const response = await fetch(fullPath, { headers: this.headers })
 
-    	this.rateLimitReset = Date.now() + Number(response.headers.get("X-Ratelimit-Reset")) * 1000
-    	this.rateLimitRemaining = Number(response.headers.get("X-Ratelimit-Remaining"))
+		this.rateLimitReset = Date.now() + Number(response.headers.get("X-Ratelimit-Reset")) * 1000
+		this.rateLimitRemaining = Number(response.headers.get("X-Ratelimit-Remaining"))
 
-    	if (response.status === 404) {
-        	return null
-    	}
+		if (response.status === 404) {
+			return null
+		}
 
-    	if (response.status !== 200) {
-        	console.log(await response.text());
-        	throw new Error(`fetch failed: ${response.status} (${response.statusText})`)
-    	}
+		if (response.status !== 200) {
+			console.log(await response.text());
+			throw new Error(`fetch failed: ${response.status} (${response.statusText})`)
+		}
 
-    	const json = await response.json()
-    	CacheManager.set(fullPath, JSON.stringify(json))
+		const json = await response.json()
+		CacheManager.set(fullPath, JSON.stringify(json))
 
-    	return json
+		return json
 	}
 }
